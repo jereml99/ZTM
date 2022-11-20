efca767a06d3f7988bada0f868511bb45c57fda8
Grzegorz Pozorski <grzegorz.pozorski@ihsmarkit.com>
2022-11-20 15:12:49 +0100

    added endpoint to get all busstops

--- Program.cs
+++ Program.cs
@@ -6,25 +6,21 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<ZTMDb>(opt => opt.UseSqlite());
 
-
-
 var app = builder.Build();
 
 app.MapGet("/login", async (ZTMDb db) =>
     await db.Users.ToListAsync());
 
-//app.MapGet("/busstops", async (ZTMDb db) =>
-//    await db.Users.ToListAsync());
-
 app.MapGet("/busstops", fetchBusStops);
 
-static async Task<object?> fetchBusStops(HttpContext context)
+static async Task<IResult> fetchBusStops()
 {
     var client = new HttpClient();
     var requestContent = (await client.GetAsync("https://ckan.multimediagdansk.pl/dataset/c24aa637-3619-4dc2-a171-a23eec8f2172/resource/4c4025f0-01bf-41f7-a39f-d156d201b82b/download/stops.json")).Content;
-    string jsonContent = requestContent.ReadAsStringAsync().Result;
-    var stopsJson = JsonConvert.DeserializeObject(jsonContent);
-    return stopsJson;
+    var jsonContent = await requestContent.ReadAsStringAsync();
+    //var stopsJson = JsonConvert.DeserializeObject(jsonContent);
+
+    return Results.Content(jsonContent);
 }
 
 app.MapPost("/adduser", async (User user, ZTMDb db) =>
